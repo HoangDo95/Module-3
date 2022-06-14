2f3ea783d57537b0ae3cd5f1f81e9f41a4f1f0b8
dotronghoang95 <dotronghoang95@gmail.com>
2022-06-14 15:57:36 +0700

    ss02

---
+++ ss02_database_design/bai_tap/b1_erg_sang_mh_bh.sql
@@ -0,0 +1,71 @@
+create database ss02_b1;
+use ss02_b1;
+
+create table phieu_xuat(
+	so_px int primary key auto_increment,
+    ngay_xuat date
+);
+
+create table vat_tu(
+	ma_vt int primary key auto_increment,
+    ten_vt varchar(50)
+);
+
+create table ct_px(
+	dg_x int,
+    sl_x int,
+    id_px int,
+    id_vt int,
+    primary key (id_px, id_vt),
+    foreign key (id_px) references phieu_xuat(so_px),
+    foreign key (id_vt) references vat_tu(ma_vt)
+);
+
+create table phieu_nhap(
+	so_pn int primary key auto_increment,
+    ngay_nhap date	
+);
+
+create table ct_pn(
+	dg_n int,
+    sl_n int,
+    id_vt int,
+    id_pn int,
+    primary key (id_vt, id_pn),
+    foreign key (id_vt) references vat_tu(ma_vt),
+    foreign key (id_pn) references phieu_nhap(so_pn)
+);
+
+create table don_dh(
+	so_dh int primary key auto_increment,
+    ngay_dh date
+);
+
+create table ct_dh(
+	id_dondh int,
+    id_vt int,
+    primary key (id_dondh, id_vt),
+    foreign key (id_dondh) references don_dh(so_dh),
+    foreign key (id_vt) references vat_tu(ma_vt)
+);
+
+create table sdt_ncc(
+	ma_ncc int primary key,
+    sdt varchar(20)
+);
+
+create table nha_cc(
+	ma_ncc int primary key auto_increment, 
+	ten_ncc varchar(50),
+    dia_chi varchar(50),
+    id_sdt_ncc int,
+    id_ncc int,
+    foreign key (id_ncc) references don_dh(so_dh),
+    foreign key (id_sdt_ncc) references sdt_ncc(ma_ncc)
+);
+
+
+
+
+
+
---
+++ ss02_database_design/bai_tap/b2_ql_ban_hang.sql
@@ -0,0 +1,36 @@
+create database ss02_b2;
+
+use ss02_b2;
+
+create table customer(
+	c_id int primary key auto_increment,
+    c_name varchar(50),
+    c_age int
+);
+
+create table `order`(
+	o_id int primary key auto_increment,
+    o_date date,
+    o_tolal_price int,
+    id_customer int,
+    foreign key (id_customer) references customer(c_id)
+);
+
+create table product(
+	p_id int primary key auto_increment,
+    p_name varchar(50),
+    p_price int
+);
+
+create table order_detail(
+	od_qty int,
+    id_order int,
+    id_product int,
+    primary key (id_order , id_product),
+    foreign key (id_order) references `order`(o_id),
+    foreign key (id_product) references product(p_id)
+);
+
+
+
+
